85dcd005b405d745e83aed9c0955f588a54c8a8b
Mostafa W@fy <113066394+Mostafa-Wafy@users.noreply.github.com>
2023-02-24 15:17:31 +0200

    third commit

--- main.cpp
+++ main.cpp
@@ -9,14 +9,18 @@ int getSum(vector<int>&vect)
     }
     return sum;
 }
-int getAverage(vector<int>&vect){
-    return  getSum(vect)/vect.size();
+int getMin(vector<int>&vect){
+    int min=vect[0];
+    for(int i=1;i<vect.size();i++){
+        if(min>vect[i])min=vect[i];
+    }
+    return min;
 }
 int main(){
     vector<int> vect(10,7);
     int sum=getSum(vect);
     cout<<sum<<endl;
-    int average=getAverage(vect);
-    cout<<average<<endl;
+    int min=getMin(vect);
+    cout<<min<<endl;
     return 0;
 }
