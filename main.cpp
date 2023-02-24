e84a5edb9d20c0f23f41e45135c00180c180eed2
Mostafa W@fy <113066394+Mostafa-Wafy@users.noreply.github.com>
2023-02-24 15:09:26 +0200

    second commit

--- main.cpp
+++ main.cpp
@@ -9,9 +9,14 @@ int getSum(vector<int>&vect)
     }
     return sum;
 }
+int getAverage(vector<int>&vect){
+    return  getSum(vect)/vect.size();
+}
 int main(){
     vector<int> vect(10,7);
     int sum=getSum(vect);
     cout<<sum<<endl;
+    int average=getAverage(vect);
+    cout<<average<<endl;
     return 0;
 }
